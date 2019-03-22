5bb7687ecba127ec85ac04738b7cbd92b9530aa7
Student <Student@DESKTOP-VMJTDJ4>
2019-03-22 10:13:37 +0900

    0322 Java

--- JavaStudy/src/ch07/Human.java
+++ JavaStudy/src/ch07/Human.java
@@ -4,6 +4,26 @@ public class Human {
 	char type;
 	 String name;
 	 int age;
+	 
+	 public char getType() {
+			return type;
+		}
+		public void setType(char type) {
+			this.type = type;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;//set에서 설정한것을 가져오는거
+		}
+		public void setAge(int age) {
+			this.age = age;//set은 설정하는거 에이지를
+		}
+
 
 	 /* setter, getter 메소드 작성 */
 
--- JavaStudy/src/ch07/Main.java
+++ JavaStudy/src/ch07/Main.java
@@ -7,11 +7,11 @@ public class Main {
 		 human.setName("김");
 		 human.setType('O');
 		 human.setAge(20);
+		 /* BloodHouse 객체 생성 */
 		 BloodHouse bh = new BloodHouse();
 		 
 
 
-		 /* BloodHouse 객체 생성 */
 		 String result = ""; // action() 메소드 호출 결과 저장
 		 result = bh.action(human);
 		 /* action() 메소드를 호출하면서 매개변수로 헌혈자 정보 넘겨줌 */
